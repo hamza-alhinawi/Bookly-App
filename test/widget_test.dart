1461c53609b7f753143b0cc66308963250fe8a69
hamza-alhinawi <hamzaalhinawi33@gmail.com>
2025-12-18 22:50:02 +0200

    ADD coreFolder && featuresFolder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,9 +11,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:my_bookly/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (
+    WidgetTester tester,
+  ) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const BooklyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
